1c63896e759693cf426048bd8ff18de2e35fd8a6
spectrecoder <pong96111kind@gmail.com>
2024-08-04 07:32:17 +0700

    fix : requirements.txt

--- run.py
+++ run.py
@@ -6,6 +6,9 @@ discord_process = subprocess.Popen(['python', 'discord_func.py'])
 # Start run.py
 telegram_process = subprocess.Popen(['python', 'telegram_func.py'])
 
+
 # Wait for both scripts to complete
 discord_process.wait()
 telegram_process.wait()
+
+print('Assistant is running')
