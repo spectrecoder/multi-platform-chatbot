614c88cf42c36a570bdc909d0876547061aa70bd
spectrecoder <pong96111kind@gmail.com>
2024-08-11 17:05:59 +0700

    feat : slack

--- run.py
+++ run.py
@@ -6,9 +6,12 @@ discord_process = subprocess.Popen(['python', 'discord_func.py'])
 # Start run.py
 telegram_process = subprocess.Popen(['python', 'telegram_func.py'])
 
+slack_process = subprocess.Popen(['python', 'slack_func.py'])
+
 
 # Wait for both scripts to complete
 discord_process.wait()
 telegram_process.wait()
+slack_process.wait()
 
 print('Assistant is running')
